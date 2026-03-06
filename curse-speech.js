ef54777e6472ec5deb997fc81af6e7bbf81c6b86
Hoochymoochy <Khaydenroberts22@gmail.com>
2026-03-05 19:09:53 -0700

    updated merge

--- curse-speech.js
+++
@@ -1,9 +0,0 @@
-const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-recognition.interimResults = true;
-recognition.onresult = (event) => {
-  const transcript = Array.from(event.results)
-    .map(result => result[0].transcript)
-    .join('');
-  console.log(transcript);
-};
-recognition.start();
